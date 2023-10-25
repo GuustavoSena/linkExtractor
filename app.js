fd35cac482334ceb31f1397699642a7edefda1e1
Gustavo Sena <GamingUnzado12@gmail.com>
2023-10-24 21:23:08 -0300

    Adding get method to test EB

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-const express = require('express');
-const { fetchHrefs } = require('./linkExtractor');
+const express = require("express");
+const { fetchHrefs } = require("./linkExtractor");
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/fetch-links', async (req, res) => {
-  const { url } = req.body;  // Get the URL from the request body
+app.get("/", (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Simple Express App</title>
+    </head>
+    <body>
+        <h1>Hello, World!</h1>
+    </body>
+    </html>
+  `);
+});
+
+app.post("/fetch-links", async (req, res) => {
+  const { url } = req.body; // Get the URL from the request body
   if (!url || !validURL(url)) {
-    return res.status(400).send({ error: 'Invalid URL' });
+    return res.status(400).send({ error: "Invalid URL" });
   }
 
   const result = await fetchHrefs(url);
-  if (result.status === 'success') {
+  if (result.status === "success") {
     res.json({ links: result.hrefs });
   } else {
     res.status(500).send({ error: result.message });
@@ -25,11 +41,14 @@ app.listen(PORT, () => {
 });
 
 function validURL(str) {
-  const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
+      "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
+      "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  ); // fragment locator
   return !!pattern.test(str);
 }
