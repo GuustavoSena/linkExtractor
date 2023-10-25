c4bb19f9e73467834b16040abc71352f4891ebd4
Gustavo Sena <GamingUnzado12@gmail.com>
2023-10-25 18:06:41 -0300

    Adding better HTML and instructions to  use API

--- app.js
+++ app.js
@@ -12,10 +12,46 @@ app.get("/", (req, res) => {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Simple Express App</title>
+        <title>Link Extractor API</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #f9f9f9;
+                color: #333;
+            }
+            .container {
+                margin: auto;
+                padding: 20px;
+                max-width: 800px;
+                background-color: #fff;
+                border-radius: 8px;
+                box-shadow: 0 0 10px rgba(0,0,0,0.1);
+            }
+            h1 {
+                font-size: 24px;
+                margin-bottom: 10px;
+            }
+            code {
+                background-color: #f0f0f0;
+                padding: 5px;
+                border-radius: 4px;
+                display: block;
+                margin-bottom: 10px;
+            }
+        </style>
     </head>
     <body>
-        <h1>Hello, World!</h1>
+        <div class="container">
+            <h1>Link Extractor API</h1>
+            <p>To extract links from a webpage, make a POST request to the following URL:</p>
+            <code>http://linksscraper-env.eba-z4jqg8m2.us-east-1.elasticbeanstalk.com/fetch-links</code>
+            <p>Include a request body with the URL from which you want to extract links, like so:</p>
+            <code>{
+  "url": "https://www.adidas-group.com/en/service/sitemap/"
+}</code>
+        </div>
     </body>
     </html>
   `);
